4d4099de9507d96ca3a753eec44337fb45c2e530
nosugerhcc <51814614+nosugerhcc@users.noreply.github.com>
2020-08-25 21:13:10 +0800

    fatree结构

---
+++ fattree/receivesc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 /home/hcc/PycharmProjects/fattree/receive.py tcp7052;
+sleep 1m
+python3 /home/hcc/PycharmProjects/fattree/receive.py tcp7065;
+sleep 1m
+python3 /home/hcc/PycharmProjects/fattree/receive.py tcp7087;
+sleep 1m
+#python3 /home/hcc/PycharmProjects/fattree/receive.py tcp70130
+#python3 /home/hcc/PycharmProjects/fattree/receive.py tcp70260
---
+++ fattree/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 /home/hcc/PycharmProjects/fattree/send.py 0.1
+python3 /home/hcc/PycharmProjects/fattree/send.py 0.01
+
+
+#python3 /home/hcc/PycharmProjects/fattree/receive.py tcp7052
+#iperf -s -u
+#iperf -c 10.0.3.3 -t 7000 -b 0.05M -u
